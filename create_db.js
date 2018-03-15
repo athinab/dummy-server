cbba59a1149b42aeb521a66f895a2230b522db69
Athina Bekakou <athinabek@grnet.gr>
2018-03-15 19:05:18 +0200

    Update create_db

--- create_db.js
+++ create_db.js
@@ -3,48 +3,96 @@ var faker = require('faker');
 function generateDB () {
   var donations = [],
       hospitals = [],
-      donation_types = [
+      coverages = [],
+      donationTypes = [
         'VOLUNTEER',
         'BLOODDONORASSOCIATION',
         'RELATIVE',
         'MILITARY'
+      ],
+      collectedComponents = [
+        'Blood',
+        'Platelets'
       ];
 
   for (var i = 0; i < 50; i++) {
     var id = i * 100 + 11;
 
-    var donated_at = faker.date.past(10),
-        donation_index = Math.floor(Math.random() * 3) + 0;
-        donation_type = donation_types[donation_index],
-        donation_id = 'donation_' + id,
-        donor_url = faker.internet.url();
+    var donatedAt = faker.date.past(10).toISOString().slice(0, 10),
+    // var donatedAt = "lalalallalalala".slice(0,10),
+        donationIndex = Math.floor(Math.random() * 4),
+        donationType = donationTypes[donationIndex],
+        donationID = 'donation_' + id,
+        coverageID = 'kalipsi_' + id,
+        collectedComponent = collectedComponents[Math.floor(Math.random() * 2)];
+        // collectedComponent = collectedComponents[0];
 
-    var hospital_id = 'hospital_' + id,
-        hospital_url = 'http://83.212.98.164:3000/hospitals/' + hospital_id,
-        hospital_name = faker.company.companyName();
+    var date = new Date(donatedAt);
+    var dd = date.getDate();
+    var mm = date.getMonth()+1;
+    var yyyy = date.getFullYear();
 
-    donations.push({
-      "id": donation_id,
-      "donated_at": donated_at,
-      "type": donation_type,
-      "hospital": hospital_url,
-      "donor": donor_url
-    });
+    if(dd<10){
+        dd='0'+dd;
+    }
+    if(mm<10){
+        mm='0'+mm;
+    }
 
-    hospitals.push({
-      "id": hospital_id,
-      "name": hospital_name
-    });
+    donatedAt = dd+'/'+mm+'/'+yyyy;
+    var hospitalID = 'hospital_' + id,
+        hospitalURL = 'http://83.212.98.164:3000/hospitals/' + hospitalID,
+        hospitalName = faker.company.companyName();
+
+    var hospital = {
+      "id": hospitalID,
+      "name": hospitalName
+    };
+
+    var donation = {
+      "id": donationID,
+      "donatedAt": donatedAt,
+      "occasion": donationType,
+      "hospitalName": hospitalName,
+      "collectedComponent": collectedComponent
+    };
+
+    var coverage = {
+      "id": coverageID,
+      "reservedAt": donatedAt,
+      "numOfBags": donationIndex + 1,
+      "hospitalName": hospitalName,
+    };
+
+    donations.push(donation);
+    coverages.push(coverage);
+    hospitals.push(hospital);
+  };
+
+
+  var config = {
+    endpoints: {
+      captcha: 'https://service.bdr.gr/blood-donor-registry-web/rest/captcha',
+      contact: 'https://service.bdr.gr/blood-donor-registry-web/rest/helpdeskissue',
+      donations: 'http://83.212.98.164:3000/donations',
+      coverages: 'http://83.212.98.164:3000/donations'
+    }
   };
 
   var profile = {
     id: 324156,
     username: faker.internet.userName(),
-    last_name: faker.name.lastName(),
-    first_name: faker.name.firstName()
+    lastName: faker.name.lastName(),
+    firstName: faker.name.firstName()
   };
 
-  return { "donations": donations, "profile": profile, "hospitals": hospitals };
+  return {
+    "donations": donations,
+    "profile": profile,
+    "hospitals": hospitals,
+    "coverages": coverages,
+    "config": config
+  };
 };
 
 module.exports = generateDB;
